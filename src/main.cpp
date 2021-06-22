925e996ee4a70e5b6e600b273e2327680ced38e6
m.klimenko <mklimenko29@gmail.com>
2021-06-22 20:02:53 +0300

    Add SignalParameters class for handling digital signal data

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
+#include "common.hpp"
+#include "signal_parameters.hpp"
+
 int main() {
-	
+	auto signal_parameters = ugsdr::SignalParameters(R"(D:\Git\ugsdr\data\iq.bin)", ugsdr::FileType::Iq_8_plus_8, 1590e6, 79.5e6 / 2);
+	auto data = signal_parameters.GetOneMs(0);
+
+	auto signal_parameters_fp32 = ugsdr::SignalParametersBase<float>(R"(D:\Git\ugsdr\data\iq.bin)", ugsdr::FileType::Iq_8_plus_8, 1590e6, 79.5e6 / 2);
+	auto data_fp32 = signal_parameters_fp32.GetOneMs(0);
+
 	
 	return 0;
 }
---
+++ src/signal_parameters.hpp
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <complex>
+#include <execution>
+#include <filesystem>
+#include <fstream>
+
+namespace ugsdr {
+	enum class FileType {
+		Iq_8_plus_8 = 0
+	};
+
+	template <typename UnderlyingType>
+	class SignalParametersBase {
+	private:
+		std::filesystem::path signal_file_path;
+		FileType file_type = FileType::Iq_8_plus_8;
+		double central_frequency = 1590e6;				// L1 central frequency
+		double sampling_rate = 0.0;
+
+		std::ifstream signal_file;
+
+		using OutputVectorType = std::vector<std::complex<UnderlyingType>>;
+
+		void OpenFile() {
+			switch (file_type) {
+			case FileType::Iq_8_plus_8:
+				signal_file = std::ifstream(signal_file_path, std::ios::binary);
+				break;
+			default:
+				throw std::runtime_error("Unexpected file type");
+			}
+		}
+
+		void GetPartialSignal(std::size_t length_samples, std::size_t samples_offset, OutputVectorType& dst) {
+			switch (file_type) {
+			case FileType::Iq_8_plus_8: {
+				std::vector<std::complex<std::int8_t>> data;
+				std::vector<std::complex<std::int8_t>>* data_ptr;
+
+				if constexpr (std::is_same_v<std::int8_t, UnderlyingType>)
+					data_ptr = &dst;
+				else
+					data_ptr = &data;
+
+				data_ptr->clear();
+				data_ptr->resize(length_samples);
+
+				signal_file.seekg(samples_offset * sizeof(std::complex<std::int8_t>));
+				signal_file.read(reinterpret_cast<char*>(data_ptr->data()), length_samples * sizeof((*data_ptr)[0]));
+
+				if constexpr (!std::is_same_v<std::int8_t, UnderlyingType>) {
+					dst.clear();
+					dst.resize(length_samples);
+					std::copy(std::execution::par_unseq, data.begin(), data.end(), dst.begin());
+				}
+
+				break;
+			}
+			default:
+				throw std::runtime_error("Unexpected file type");;
+			}
+		}
+
+	public:
+		SignalParametersBase(std::filesystem::path signal_file, FileType type, double central_freq, double sampling_freq) :
+			signal_file_path(std::move(signal_file)),
+			file_type(type),
+			central_frequency(central_freq),
+			sampling_rate(sampling_freq) {
+			OpenFile();
+		}
+
+		void GetOneMs(std::size_t ms_offset, OutputVectorType& dst) {
+			const auto samples_per_ms = static_cast<std::size_t>(sampling_rate / 1000);
+			GetPartialSignal(samples_per_ms, samples_per_ms * ms_offset, dst);
+		}
+
+		auto GetOneMs(std::size_t ms_offset) {
+			auto dst = OutputVectorType(static_cast<std::size_t>(sampling_rate / 1e3));
+			GetOneMs(ms_offset, dst);
+			return dst;
+		}
+
+	};
+
+	using SignalParameters = SignalParametersBase<std::int8_t>;
+}
