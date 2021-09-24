1736004b6007c1fec8d44f190a94bcdf72cab7be
m.klimenko <mklimenko29@gmail.com>
2021-09-24 23:48:28 +0300

    Add DFT benchmark

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -5,6 +5,10 @@
 #include "../src/mixer/batch_mixer.hpp"
 #include "../src/mixer/ipp_mixer.hpp"
 
+#include "../src/math/af_dft.hpp"
+#include "../src/math/ipp_dft.hpp"
+#include "../src/helpers/ipp_complex_type_converter.hpp"
+
 #include <execution>
 #include <random>
 
@@ -61,49 +65,95 @@ namespace mixer {
 }
 #endif
 
-static void Multiply(benchmark::State& state) {
-    std::vector<Ipp32fc> a(state.range());
-    std::vector<Ipp32fc> b(state.range());
-    std::vector<Ipp32fc> c(state.range());
-    for (auto _ : state) {
-        ippsMul_32fc(a.data(), b.data(), c.data(), static_cast<int>(a.size()));
-        benchmark::DoNotOptimize(a);
-        benchmark::DoNotOptimize(b);
-        benchmark::DoNotOptimize(c);
-    }
-    state.SetComplexityN(state.range());
-}
-BENCHMARK(Multiply)->RangeMultiplier(2)->Range(2048, 2048 << 6)->Complexity();
+#if 0
+namespace ipp_math {
+	static void Multiply(benchmark::State& state) {
+	    std::vector<Ipp32fc> a(state.range());
+	    std::vector<Ipp32fc> b(state.range());
+	    std::vector<Ipp32fc> c(state.range());
+	    for (auto _ : state) {
+	        ippsMul_32fc(a.data(), b.data(), c.data(), static_cast<int>(a.size()));
+	        benchmark::DoNotOptimize(a);
+	        benchmark::DoNotOptimize(b);
+	        benchmark::DoNotOptimize(c);
+	    }
+	    state.SetComplexityN(state.range());
+	}
+	BENCHMARK(Multiply)->RangeMultiplier(2)->Range(2048, 2048 << 6)->Complexity();
 
-static void ConjPlusMultiply(benchmark::State& state) {
-    std::vector<Ipp32fc> a(state.range());
-    std::vector<Ipp32fc> b(state.range());
-    std::vector<Ipp32fc> c(state.range());
-    for (auto _ : state) {
-        ippsConj_32fc_I(a.data(), static_cast<int>(a.size()));
-        ippsMul_32fc(a.data(), b.data(), c.data(), static_cast<int>(a.size()));
-        benchmark::DoNotOptimize(a);
-        benchmark::DoNotOptimize(b);
-        benchmark::DoNotOptimize(c);
-    }
-    state.SetComplexityN(state.range());
-}
-BENCHMARK(ConjPlusMultiply)->RangeMultiplier(2)->Range(2048, 2048 << 6)->Complexity();
+	static void ConjPlusMultiply(benchmark::State& state) {
+	    std::vector<Ipp32fc> a(state.range());
+	    std::vector<Ipp32fc> b(state.range());
+	    std::vector<Ipp32fc> c(state.range());
+	    for (auto _ : state) {
+	        ippsConj_32fc_I(a.data(), static_cast<int>(a.size()));
+	        ippsMul_32fc(a.data(), b.data(), c.data(), static_cast<int>(a.size()));
+	        benchmark::DoNotOptimize(a);
+	        benchmark::DoNotOptimize(b);
+	        benchmark::DoNotOptimize(c);
+	    }
+	    state.SetComplexityN(state.range());
+	}
+	BENCHMARK(ConjPlusMultiply)->RangeMultiplier(2)->Range(2048, 2048 << 6)->Complexity();
 
 
-static void MultiplyByConj(benchmark::State& state) {
-    std::vector<Ipp32fc> a(state.range());
-    std::vector<Ipp32fc> b(state.range());
-    std::vector<Ipp32fc> c(state.range());
-    for (auto _ : state) {
-        ippsMulByConj_32fc_A24(a.data(), b.data(), c.data(), static_cast<int>(a.size()));
-        benchmark::DoNotOptimize(a);
-        benchmark::DoNotOptimize(b);
-        benchmark::DoNotOptimize(c);
-    }
-    state.SetComplexityN(state.range());
+	static void MultiplyByConj(benchmark::State& state) {
+	    std::vector<Ipp32fc> a(state.range());
+	    std::vector<Ipp32fc> b(state.range());
+	    std::vector<Ipp32fc> c(state.range());
+	    for (auto _ : state) {
+	        ippsMulByConj_32fc_A24(a.data(), b.data(), c.data(), static_cast<int>(a.size()));
+	        benchmark::DoNotOptimize(a);
+	        benchmark::DoNotOptimize(b);
+	        benchmark::DoNotOptimize(c);
+	    }
+	    state.SetComplexityN(state.range());
+	}
+	BENCHMARK(MultiplyByConj)->RangeMultiplier(2)->Range(2048, 2048 << 6)->Complexity();
 }
-BENCHMARK(MultiplyByConj)->RangeMultiplier(2)->Range(2048, 2048 << 6)->Complexity();
+#endif
+
+namespace dft {
+    constexpr auto max_range = 2048 << 8;
+	
+#define DFT_BENCHMARK_OPTIONS RangeMultiplier(2)->Range(2048, max_range)->Complexity()
+	
+    template <typename T>
+    static void Ipp(benchmark::State& state) {
+        using DftType = ugsdr::DiscreteFourierTransform<ugsdr::IppDft>;
+
+        std::vector<std::complex<T>> vec(max_range);
+        DftType::Transform(vec);
+        vec.resize(state.range());
+
+        for (auto _ : state) {
+            DftType::Transform(vec);
+            benchmark::DoNotOptimize(vec);
+        }
+        state.SetComplexityN(state.range());
+    }
+    BENCHMARK_TEMPLATE(Ipp, float)->DFT_BENCHMARK_OPTIONS;
+    //BENCHMARK_TEMPLATE(Ipp, double)->DFT_BENCHMARK_OPTIONS;
+
+
+    template <typename T>
+    static void Af(benchmark::State& state) {
+        using DftType = ugsdr::DiscreteFourierTransform<ugsdr::AfDft>;
+
+        std::vector<std::complex<T>> vec(max_range);
+        DftType::Transform(vec);
+        vec.resize(state.range());
+
+        for (auto _ : state) {
+            DftType::Transform(vec);
+            benchmark::DoNotOptimize(vec);
+        }
+        state.SetComplexityN(state.range());
+    }
+    BENCHMARK_TEMPLATE(Af, float)->DFT_BENCHMARK_OPTIONS;
+    BENCHMARK_TEMPLATE(Af, double)->DFT_BENCHMARK_OPTIONS;
+
+}
 
 int main(int argc, char** argv) {
     ::benchmark::Initialize(&argc, argv);
