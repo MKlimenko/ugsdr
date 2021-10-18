e576a7396a74535d48afff3a1f5810337530cd49
m.klimenko <mklimenko29@gmail.com>
2021-10-19 00:33:22 +0300

    Use GoogleBenchmark from vcpkg instead of downloaded one (filter the tests)

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,12 @@
 ﻿#include "gtest/gtest.h"
 
+#include "../src/correlator/af_correlator.hpp"
+#include "../src/correlator/ipp_correlator.hpp"
+
+TEST(correlators, ipp_correlator) {
+	
+}
+
 TEST(placeholder_tests, placeholder) {
 	ASSERT_TRUE(true);
 }
